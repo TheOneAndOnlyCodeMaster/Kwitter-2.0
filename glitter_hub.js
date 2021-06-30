c8dba2b4a7493d5987655ef62ba461001bf4dda9
TheOneAndOnlyCodeMaster <74931617+TheOneAndOnlyCodeMaster@users.noreply.github.com>
2021-06-30 09:28:35 -0400

    Add files via upload

--- glitter_hub.js
+++ glitter_hub.js
@@ -12,10 +12,8 @@ firebase.initializeApp(firebaseConfig);
 username = localStorage.getItem("username");
 document.getElementById("user_name").innerHTML ="Welcome to Glitter " + username + "!";
 function addRoom(){
-  room_name = document.getElementById("room_name").value;
+  room_name = document.getElementById("room_name_input").value;
   console.log("room_name = "+room_name);
-  row = "<div class='room_name' id='"+Room_names+"' onclick='next_page(this.id)'> #"+Room_names +" </div><hr>";
-  document.getElementById("output").innerHTML = row;
   firebase.database().ref("/").child(room_name).update({
     "room_name": room_name
   })
@@ -28,8 +26,13 @@ function getData() {
 firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
 Room_names = childKey;
  //Start code
-
+ row = "<div class='room_name' id='"+Room_names+"' onclick='next_page(this.id)'> #"+Room_names +" </div><hr>";
+ document.getElementById("output").innerHTML += row;
  //End code
  });});}
 
  getData();
+function next_page(name){
+  localStorage.setItem("room_name", name);
+  window.location = "room_page.html";
+}
